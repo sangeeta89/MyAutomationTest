1eab5ff6a1f8b30d07261a686bf60ece8cb1eca2
unknown <pulak.60choudhury@gmail.com>
2018-01-20 16:34:32 +0530

    changes

--- demo/src/main/java/DemoTest/demo/App.java
+++ demo/src/main/java/DemoTest/demo/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello S!" );
         System.out.println("Automation");
         System.out.println("My Name is Pulak");
     }
